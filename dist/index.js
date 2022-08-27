1ab5f9f0ff2b392fb28032a541483985dae343ce
Hoang Vo <dev+g@hvvo.dev>
2022-08-28 00:05:24 +0700

    Generate dist

--- dist/index.js
+++ dist/index.js
@@ -19193,6 +19193,7 @@ exports.executePostRelease = async function executePostRelease() {
     ...Config.repo,
     tag_name: version,
     target_commitish: Config.prodBranch,
+    name: releaseNotes.name,
     body: releaseNotes.body,
   });
 
@@ -19202,8 +19203,12 @@ exports.executePostRelease = async function executePostRelease() {
 
   const slackStr = core.getInput("slack");
   if (slackStr) {
-    const slackOpts = JSON.parse("slack");
-
+    let slackOpts;
+    try {
+      slackOpts = JSON.parse(slackStr);
+    } catch (err) {
+      throw new Error(`Could not parse ${slackStr}`);
+    }
     console.log(`Posting to slack channel #${slackOpts.channel}`);
     const slackToken = process.env.SLACK_TOKEN;
     if (!slackToken) throw new Error("process.env.SLACK_TOKEN is not defined");
